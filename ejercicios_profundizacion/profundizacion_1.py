188545975bb3576668e963f05c11565c9e548892
martin1422 <martinecassino35@gmail.com>
2022-06-18 20:33:56 -0300

    Ejerciocio profundizacion1 listo

--- ejercicios_profundizacion/profundizacion_1.py
+++ ejercicios_profundizacion/profundizacion_1.py
@@ -29,5 +29,49 @@ E) Exponente/Potencia
   ej: La suma entre 4.2 y 6.5 es 10.7
 '''
 
-print('¡Nuestra primera calculadora!')
+print('¡Nuestra primera calculadora!\n Eliga una opcion:')
 # Empezar aquí la resolución del ejercicio
+opcion = ''
+while(opcion != 'F'):
+  print("A) Suma\nB) Resta\nC) Multiplicacion\D) Division\nE) Exponente/Potencia\nF) Salir")
+  opcion = str(input())
+  if(opcion == 'A'):
+    print('Ingrese numero 1: ')
+    numero_1 = int(input())
+    print('\nIngrese numero 2: ')
+    numero_2 = int(input())
+    suma = numero_1 + numero_2
+    print('La suma entre ' + str(numero_1) + ' y ' + str(numero_2) + ' es = ' + str(suma))
+
+  if(opcion == 'B'):
+    print('Ingrese numero 1: ')
+    numero_1 = int(input())
+    print('\nIngrese numero 2: ')
+    numero_2 = int(input())
+    resta = numero_1 -  numero_2
+    print('La resta entre ' + str(numero_1) + ' y ' + str(numero_2) + ' es = ' + str(resta))
+
+  if(opcion == 'C'):
+    print('Ingrese numero 1: ')
+    numero_1 = int(input())
+    print('\nIngrese numero 2: ')
+    numero_2 = int(input())
+    mul = numero_1 * numero_2
+    print('La multiplicacion entre ' + str(numero_1) + ' y ' + str(numero_2) + ' es = ' + str(mul))
+
+  if(opcion == 'D'):
+    print('Ingrese numero 1: ')
+    numero_1 = int(input())
+    print('\nIngrese numero 2: ')
+    numero_2 = int(input())
+    div = numero_1 / numero_2
+    print('La Divicion entre ' + str(numero_1) + ' y ' + str(numero_2) + ' es = ' + str(div)) 
+
+  if(opcion == 'E'):
+    print('Ingrese la base: ')
+    numero_1 = int(input())
+    print('\nIngrese el exponente: ')
+    numero_2 = int(input())
+    exp = numero_1 ** numero_2
+    print('La potenciacion entre ' + str(numero_1) + ' y ' + str(numero_2) + ' es = ' + str(exp))
+
